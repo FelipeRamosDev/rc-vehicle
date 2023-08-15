afdf4ba88cb1e56d619adbffe6728e475cdc67b3
FelipeRamosDev <felipe@feliperamos.info>
2023-08-15 10:00:07 -0300

    [RCV-3] API Server Architectture

--- src/Services/API/index.js
+++ src/Services/API/index.js
@@ -1,5 +1,13 @@
+import express from 'express';
+
 export default class API {
-    constructor () {
-        
+    constructor ({
+        port,
+        onListen
+    }) {
+        this.server = express();
+
+        this.server.use(express.static('public'));
+        this.server.listen(port || 80, onListen);
     }
 }
