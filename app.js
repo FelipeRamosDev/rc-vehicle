c68d13f860e9b3892f4aa064b30231c39e13cfe1
FelipeRamosDev <felipe@feliperamos.info>
2023-08-15 11:13:23 -0300

    [RCV-4] Saving changes did so far

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import API from './src/Services/API/index.js';
+import SocketIO from './src/Services/SocketIO/index.js';
 
 class App {
     constructor () {
         this.api = new API({
             onListen: () => console.log('API server is running!')
         });
+
+        this.socketIO = new SocketIO();
     }
 }
 
